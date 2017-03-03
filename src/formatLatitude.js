ab28ac520987673838f447212fc77049059d0820
Dmitriy Pushkov <ezze@ezze.org>
2017-03-04 00:01:04 +0300

    value and negativeSign tokens are supported by formatLatitude and formatLongitude functions.

--- src/formatLatitude.js
+++ src/formatLatitude.js
@@ -13,10 +13,12 @@ export default function formatLatitude(value, template, isDegrees) {
     const f = getAngleFormatObject(value, isDegrees);
 
     return formatByTemplate(template, {
+        value: f.value,
         degree: prependChars(f.degree, 2, '0'),
         prime: prependChars(f.prime, 2, '0'),
         doublePrime: prependChars(f.doublePrime, 2, '0'),
         sign: (f.sign > 0 ? '+' : (f.sign < 0 ? '—' : '')),
+        negativeSign: f.sign < 0 ? '—' : '',
         direction: f.sign >= 0 ? 'N' : 'S'
     });
 }
