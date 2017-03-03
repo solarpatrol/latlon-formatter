ae49ab66e7ea6f5f6e4441d8428e9f27726da9b5
Dmitriy Pushkov <ezze@ezze.org>
2017-03-04 00:50:41 +0300

    formatAngle function is rewritten and covered by tests.

--- src/formatAngle.js
+++ src/formatAngle.js
@@ -1,4 +1,23 @@
-export default function formatAngle(value, fixed) {
-    fixed = fixed || 0;
-    return parseFloat(value).toFixed(fixed) + ' °';
+import getAngleFormatObject from './getAngleFormatObject';
+import prependChars from './prependChars';
+import formatByTemplate from './formatByTemplate';
+
+const defaultTemplate = '{negativeSign}{value}°';
+
+export default function formatAngle(value, options) {
+    options = options || {};
+    const template = typeof options.template === 'string' ? options.template : defaultTemplate;
+    const isDegrees = typeof options.isDegrees === 'boolean' ? options.isDegrees : false;
+    const fixedCount = typeof options.fixedCount === 'number' ? options.fixedCount : 0;
+
+    const f = getAngleFormatObject(value, isDegrees);
+
+    return formatByTemplate(template, {
+        value: f.value.toFixed(fixedCount),
+        degree: f.degree,
+        prime: prependChars(f.prime, 2, '0'),
+        doublePrime: prependChars(f.doublePrime, 2, '0'),
+        sign: (f.sign > 0 ? '+' : (f.sign < 0 ? '—' : '')),
+        negativeSign: f.sign < 0 ? '—' : ''
+    });
 }
--- test/formatAngle.test.js
+++ test/formatAngle.test.js
@@ -1,1 +1,91 @@
-//import formatAngle from '../src/formatAngle';
+import chai from 'chai';
+chai.should();
+
+import formatAngle from '../src/formatAngle';
+
+describe('format angle', () => {
+    it('zero (radians)', () => {
+        formatAngle(0).should.equal('0°');
+    });
+
+    it('zero (degrees)', () => {
+        formatAngle(0, {
+            isDegrees: true
+        }).should.equal('0°');
+    });
+
+    it('positive integer (radians)', () => {
+        formatAngle(Math.PI / 3).should.equal('60°');
+    });
+
+    it('positive integer (degrees)', () => {
+        formatAngle(60, {
+            isDegrees: true
+        }).should.equal('60°');
+    });
+
+    it('negative integer (radians)', () => {
+        formatAngle(-Math.PI / 3).should.equal('—60°');
+    });
+
+    it('negative integer (degrees)', () => {
+        formatAngle(-60, {
+            isDegrees: true
+        }).should.equal('—60°');
+    });
+
+    it('positive float (radians)', () => {
+        formatAngle(Math.PI / 40).should.equal('5°');
+    });
+
+    it('positive float (degrees)', () => {
+        formatAngle(4.5, {
+            isDegrees: true
+        }).should.equal('5°');
+    });
+
+    it('negative float (radians)', () => {
+        formatAngle(-Math.PI / 40).should.equal('—5°');
+    });
+
+    it('negative float (degrees)', () => {
+        formatAngle(-4.5, {
+            isDegrees: true
+        }).should.equal('—5°');
+    });
+
+    it('positive float (radians) with 3 digits precision', () => {
+        formatAngle(Math.PI / 40, {
+            fixedCount: 3
+        }).should.equal('4.500°');
+    });
+
+    it('positive float (degrees) with 3 digits precision', () => {
+        formatAngle(4.5666, {
+            isDegrees: true,
+            fixedCount: 3
+        }).should.equal('4.567°');
+    });
+
+    it('negative float (radians) with 3 digits precision', () => {
+        formatAngle(-Math.PI / 40, {
+            fixedCount: 3
+        }).should.equal('—4.500°');
+    });
+
+    it('negative float (degrees) with 3 digits precision', () => {
+        formatAngle(-4.5666, {
+            isDegrees: true,
+            fixedCount: 3
+        }).should.equal('—4.567°');
+    });
+
+    it('positive float with custom template', () => {
+        const template = '{degree}° {prime}′ {doublePrime}″';
+        const expected = '14° 49′ 12″';
+        formatAngle(14.82, {
+            template,
+            isDegrees: true
+        }).should.equal(expected);
+    });
+});
